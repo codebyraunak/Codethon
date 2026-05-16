fd8009f33da6d34fc30b2265608897ec5caa5598
Raunak Kumar <rkaunak957@gmail.com>
2026-05-17 00:19:52 +0530

    Add Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getDatabase, ref, set, get, onValue } from "firebase/database";
 
 // TODO: Replace with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyArnUXYEcB_h55eEx33PKaQW0b0QfFsP9Y",
+  authDomain: "codethon-34ed5.firebaseapp.com",
+  databaseURL: "https://codethon-34ed5-default-rtdb.firebaseio.com",
+  projectId: "codethon-34ed5",
+  storageBucket: "codethon-34ed5.firebasestorage.app",
+  messagingSenderId: "435171073837",
+  appId: "1:435171073837:web:b9abd366044105e2837da4",
+  measurementId: "G-69KQLYWMEL"
 };
 
 // Initialize Firebase
